490aa23f24ecde278c5dc6053393f368aae63164
idkngrid <ingrid.camargopr@gmail.com>
2023-10-02 20:21:03 -0300

    style: disable some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,7 @@ module.exports = {
     'class-methods-use-this': 'off',
     'consistent-return': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: false, optionalDependencies: false, peerDependencies: false }],
   },
 };
